30bd1ed19fe7eae0e086ee9c203ddce90cdb0e87
Arthur Ice <addtheice@gmail.com>
2016-03-01 15:37:38 -0800

    Further formatting and adding of k|Ex|Hy table header

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,12 @@ const KELEMENTCOUNT: usize = 200;
 /// delta / ex table header.
 static EXTABLEDIVIDER: &'static str = "-------------------------------------------------";
 
+/// Just a static string for the divider header for the
+/// K | Ex | Hy table header.
+static EXKXTABLEDIVIDER: &'static str = "---------------------------------------------";
+
 /// Prints the delta | ex[kc] header with divider.
-/// This also prints a new line before th header
+/// This also prints a new line before the header
 /// inorder to give room between the previous
 /// inputs or headers.
 fn print_delta_ex_table_header() {
@@ -20,6 +24,17 @@ fn print_delta_ex_table_header() {
     println!("{}", EXTABLEDIVIDER);
 }
 
+/// Prints the k | Ex | Hy header with divider.
+/// This also prints a new line before the header
+/// inorder to give room between the previous
+/// inputs or headers.
+fn print_ex_hy_table_header() {
+    // print the k | ex[kc] | hy[kc] table header.
+    println!("");
+    println!("{0:<03} | {1:<040} | {2:<040}", "k", "Ex[k]", "Hy[k]");
+    print!("{}", EXKXTABLEDIVIDER);
+    println!("{}", EXKXTABLEDIVIDER);
+}
 
 /// This is a mostly direct translation of the FD1D_1.1.C
 /// 1D FDTD simulation in free space program found at the
@@ -73,40 +88,41 @@ fn main () {
             let delta: f64 = pulse_t0 - tick;
             let pulse = (-0.5f64 * (delta/SPREAD).powf(2.0f64) ).exp();
             ex[kc] = pulse;
-            println!("{0:<6} | {1:<040}", delta, ex[kc]);
+            println!("{0:<6} | {1:<40}", delta, ex[kc]);
 
             // Calculate the Hy field.
             for k in 0..KELEMENTCOUNT - 1 {
                 hy[k] = hy[k] + 0.5f64 * (ex[k] - ex[k+1]);
             }
         }
-
         // End of the Main FDTD Loop.
+
         // At the end of the calculation, print out
         // the Ex and Hy fields.
+        print_ex_hy_table_header();
         for k in 0..KELEMENTCOUNT -1 {
-            println!("{0:<03} | {1:<040} | {2:<040}", k, ex[k], hy[k]);
+            println!("{0:<3} | {1:<40} | {2:<40}", k, ex[k], hy[k]);
         }
 
         // Write the E field out to a file "Ex".
         let mut e_field_file = File::create("Ex.txt").unwrap();
         for k in 1..KELEMENTCOUNT {
-            writeln!(e_field_file, "{:<040}", ex[k]);
+            writeln!(e_field_file, "{:<40}", ex[k]);
         }
 
         // Write the H field out to a file "Hy".
         let mut h_field_file = File::create("Hy.txt").unwrap();
         for k in 1..KELEMENTCOUNT {
-            writeln!(h_field_file, "{:<040}", hy[k]);
+            writeln!(h_field_file, "{:<40}", hy[k]);
         }
 
-        println!("Tick count = {0:<04}", tick);
+        println!("Tick count = {0:<4}", tick);
 
         print!("Number Of Steps: ");
         let _ = io::stdout().flush();
         let mut input_buffer = String::new();
         let _ = io::stdin().read_line(&mut input_buffer);
-        let mut number_of_steps = input_buffer.trim().parse::<i32>().unwrap();
+        number_of_steps = input_buffer.trim().parse::<i32>().unwrap();
 
     }
 }
