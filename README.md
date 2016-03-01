ef2a136afa32c3912f045d351aea49381e697722
Arthur Ice <addtheice@gmail.com>
2016-03-01 02:55:12 -0800

    Create README.md

---
+++ README.md
@@ -0,0 +1,5 @@
+# simulate
+Problem sets for the book "Electromagnetic Simulation Using the FDTD Method, 2nd Edition" in Rust instead of C
+
+I'm still not sure how this project will develop but it should have some of my thinking and workings while
+reading the book and learning the FDTD methodology.
--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,28 @@ use std::io::Write;
 
 const KELEMENTCOUNT: usize = 200;
 
+/// This is a mostly direct translation of the FD1D_1.1.C
+/// 1D FDTD simulation in free space program found at the
+/// end of chapter 1.
+///
+/// The major differences are that I've converted the code
+/// to rust, formatted it to follow rust styling, changed
+/// some of the variables to match a more 'rustonic'
+/// styling and removed any unused variables.
+///
+/// Currently this program does not follow rust styling
+/// in regards to input validation and sanitization and
+/// skips all forms of error checking and modularity.
+///
+/// This works as an example program, but in the future
+/// expanding to a real time graphing, expanded sizes,
+/// and GPU processing will occur in multiple dimensions.
+///
+/// For now, this works as intended as an example as to
+/// the technique.
+///
+/// As new programs are added, this file will be moved
+/// into a sub folder.
 fn main () {
     let mut ex: [f64; KELEMENTCOUNT] = [0.0f64; KELEMENTCOUNT];
     let mut hy: [f64; KELEMENTCOUNT] = [0.0f64; KELEMENTCOUNT];
@@ -28,37 +50,37 @@ fn main () {
         for _ in 1..number_of_steps + 1 {
             tick = tick + 1.0f64;
             // Main FDTD Loop
-            // Calculate the Ex field
+            // Calculate the Ex field.
             for k in 1..KELEMENTCOUNT {
                 ex[k] = ex[k] + 0.5 * (hy[k-1] - hy[k]);
             }
 
-            /* Put a Gaussian pulse in the middle */
+            // Put a Gaussian pulse in the middle.
             let delta: f64 = pulse_t0 - t0;
             let pulse = (-0.5f64 * (delta/SPREAD).powf(2.0f64) ).exp();
             ex[kc] = pulse;
             println!("{0:>010} {1:>010}", delta, ex[kc]);
 
-            /* Calculate the Hy field */
+            // Calculate the Hy field.
             for k in 0..KELEMENTCOUNT - 1 {
                 hy[k] = hy[k] + 0.5f64 * (ex[k] - ex[k+1]);
             }
         }
 
-        /* End of the Main FDTD Loop */
-        /* At the end of the calculation, print out
-        the Ex and Hy fields */
+        // End of the Main FDTD Loop.
+        // At the end of the calculation, print out
+        // the Ex and Hy fields.
         for k in 0..KELEMENTCOUNT -1 {
             println!("{0:>03} {1:>010} {2:>010}", k, ex[k], hy[k]);
         }
 
-        /* Write the E field out to a file "Ex" */
+        // Write the E field out to a file "Ex".
         let mut e_field_file = File::create("Ex.txt").unwrap();
         for k in 1..KELEMENTCOUNT {
             writeln!(e_field_file, "{:<062}", ex[k]);
         }
 
-        /* Write the H field out to a file "Hy" */
+        // Write the H field out to a file "Hy".
         let mut h_field_file = File::create("Hy.txt").unwrap();
         for k in 1..KELEMENTCOUNT {
             writeln!(h_field_file, "{:<062}", hy[k]);
