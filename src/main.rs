2d3b46b98ed2019b2e82e538d22e621d77f2755e
Arthur Ice <addtheice@gmail.com>
2016-03-04 00:11:55 -0800

    Switched to multi charting for gnuplot

--- src/main.rs
+++ src/main.rs
@@ -77,17 +77,20 @@ fn main () {
             .set_title("Figure 1.2", &[])
             .set_x_label("FDTD cells", &[])
             .set_y_label("Ex", &[])
-            .set_x_ticks(Some((Fix(20.0), 0)), &[Mirror(false)], &[])
+            .set_size(1.0, 0.50)
+            .set_pos(0.0,0.5)
+            .set_y_ticks(Some((Fix(1.0), 0)), &[Mirror(false)], &[])
+            .set_x_ticks(Some((Fix(20.0), 0)), &[Mirror(true)], &[])
             .lines(0..200, ex.iter(), &[Color("black")]);
-        figure.show();
 
         // Produce the gnuplot chart of the Hy pulse.
-        let mut figure = Figure::new();
         figure.axes2d()
             .set_title("Figure 1.2", &[])
             .set_x_label("FDTD cells", &[])
             .set_y_label("Hy", &[])
-            .set_x_ticks(Some((Fix(20.0), 0)), &[Mirror(false)], &[])
+            .set_size(1.0, 0.50)
+            .set_y_ticks(Some((Fix(1.0), 0)), &[Mirror(false)], &[])
+            .set_x_ticks(Some((Fix(20.0), 0)), &[Mirror(true)], &[])
             .lines(0..200, hy.iter(), &[Color("black")]);
         figure.show();
 
